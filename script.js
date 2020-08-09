d429483a7149433f39eea58f0e1ec9ae12eecd71
Johntae Stroud <johntae.stroud@gmail.com>
2020-08-09 07:39:05 -0400

    Display Data

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = 'vPZYloj90qvv-1Pt-HcwiJRd900ManlYn97idQIPoUc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper Function to Set Attribute on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    // setting href with link
+    // item.setAttribute('href', photo.links.html);
+    // opening window in a new tab
+    // item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // create <img> for photo
+    const img = document.createElement('img');
+    // The src is going to load the image so we start there
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
+    // Put <img> inside <a> then puth both inside imageContainer 
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
       const response = await fetch(apiUrl);
-      const data = await response.json(); // run through the json method and be returned as json
-      console.log(data);
+      // run through the json method and be returned as json
+      photosArray = await response.json(); 
+      displayPhotos();
+
     } catch (error) {
         // Catch error
     }
